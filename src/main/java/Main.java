3c70575ff804e187567d483f26deae12650b2533
Mike <mike@Zipcoders-MacBook-Pro-41.local>
2022-06-27 10:42:29 -0400

    enter an integer n to get the sum of 1-n

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner scan = new Scanner((System.in));
+        int n = 0;
+        int sum = 0;
+        int i;
+        System.out.println("Enter an integer...");
+        n = scan.nextInt();
+        for (i = 0; i<=n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
